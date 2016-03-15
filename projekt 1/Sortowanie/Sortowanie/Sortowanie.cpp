f845f1cac7356cdcd39ecf84bb84c169ea10361d
dochman <daniel.ochman95@gmail.com>
2016-03-15 19:06:45 +0100

    Sortowanie bąbelkowe zakończone, teraz dorobić następne, struktura środowiska zrobiona, trzeba weryfikację dorobić

--- projekt 1/Sortowanie/Sortowanie/Sortowanie.cpp
+++ projekt 1/Sortowanie/Sortowanie/Sortowanie.cpp
@@ -11,51 +11,76 @@ void wypelnienie_tablicy(int *t, int n)
 	{
 		t[i] = i++;
 	}
+	return;
 }
 
-int sortowanie_babelkowe(int *t, int n)
+void zwieksz_ilosc_danych(int *&t, int &rozmiar)
 {
-	int rez,czas;
-	clock_t start, stop;
+	delete[] t;
+	rozmiar = rozmiar * 2;
+	t = new int[rozmiar];
+	wypelnienie_tablicy(t, rozmiar);
+	return;
+}
 
-	start = clock();
+void sortowanie_babelkowe(int *t, int n)
+{
+	int rez;
 
 	for (int i = 0; i < n; i++)
 		for (int j = 1; j < n; j++)
 		{
 			if (t[j - 1] > t[j])
 			{
-				rez = t[j - 1];
+				/*rez = t[j - 1];
 				t[j - 1] = t[j];
-				t[j] = rez;
+				t[j] = rez;*/
+				swap(t[j], t[j - 1]);
 			}
 		}
-
-	stop = clock();
-	czas = stop - start;
-	return czas;
+	return;
 }
 
 int main()
 {
 	//tablica dynamiczna
-	int n=8000; 
+	int n=1000; 
 	int* t = new int[n];  
 	float suma = 0;
+	float srednia = 0;
+	int czas,licznik=1;
+	clock_t start, stop;
 
-	for (int i = 1; i <= 5; i++)
-	{
-		wypelnienie_tablicy(t, n);
-		random_shuffle(t, t + n);
-		//cout << "czas w sekundach " <<i<<" pomiar: "<< ((float)sortowanie_babelkowe(t, n)) / CLOCKS_PER_SEC << endl;
-		suma = suma + ((float)sortowanie_babelkowe(t, n)) / CLOCKS_PER_SEC;
-	}
-	cout << "sredni czas-babelkowe: " << suma / 5<<endl;
-
-	/*const int n =1000;
-	int t[n];
 	wypelnienie_tablicy(t, n);
-	random_shuffle(t, t + n);
-	cout<<"czas w sekundach: "<<((float)sortowanie_babelkowe(t, n))/CLOCKS_PER_SEC<<endl;
-	*/
+
+	while (true)
+	{
+		suma = 0;
+		for (int i = 1; i <= 5; ++i)
+		{
+			wypelnienie_tablicy(t, n);
+			random_shuffle(t, t + n);
+			//czas = 0;
+
+			start = clock();
+			sortowanie_babelkowe(t, n);
+			stop = clock();
+			czas = stop-start;
+
+			suma += czas / (float)CLOCKS_PER_SEC;
+		}
+		srednia = suma /= 5;
+		cout <<licznik<<"*10^3: "<<"sredni czas-babelkowe= " << srednia << endl;
+		licznik *= 2;
+
+		if (srednia > 30)
+		{
+			cout << "Przekroczono 30 sekund" << endl;
+			break;
+		}
+		zwieksz_ilosc_danych(t, n);
+	}
+
+	delete[] t;
+	return 0;
 }
